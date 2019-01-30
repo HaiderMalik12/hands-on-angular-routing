c8f9e817f1689ffb5ef72976de98a5036c51cafd
HaiderMalik12 <haidermalik504@gmail.com>
2019-01-30 10:36:08 +0500

    02-module import order deos matter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PostsModule } from './posts/posts.module';
 const appRoutes: Routes = [
   {
     path: 'about',
@@ -13,7 +14,8 @@ const appRoutes: Routes = [
 ];
 @NgModule({
   declarations: [],
-  imports: [CommonModule, RouterModule.forRoot(appRoutes)],
+  // https://angular.io/guide/router#module-import-order-matters
+  imports: [CommonModule, PostsModule, RouterModule.forRoot(appRoutes)], // Module import order does matter here
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
