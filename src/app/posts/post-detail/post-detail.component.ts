5956119f1555b00dbd485a432ee7bed3dc534142
HaiderMalik12 <haidermalik504@gmail.com>
2019-01-30 10:56:26 +0500

    02-fetch route param using paramMap and switchMap

--- src/app/posts/post-detail/post-detail.component.ts
+++ src/app/posts/post-detail/post-detail.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import { Post } from '../post';
+import { Observable } from 'rxjs';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-detail',
@@ -7,11 +12,21 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./post-detail.component.css']
 })
 export class PostDetailComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
-
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {}
+  post$: Observable<Post>;
   ngOnInit() {
     // activated route without observable
     // https://angular.io/guide/router#parammap-api
-    console.log(this.route.snapshot.paramMap.get('id'));
+    // console.log(this.route.snapshot.paramMap.get('id'));
+
+    // activated route with observable
+    this.post$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.postService.getPost(params.get('id'))
+      )
+    );
   }
 }
