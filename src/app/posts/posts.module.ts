322271d251d3a9613909954179f64808304b8ea1
HaiderMalik12 <haidermalik504@gmail.com>
2019-01-30 10:29:05 +0500

    02-create post-list and post-detail component

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PostsRoutingModule } from './posts-routing.module';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PostListComponent, PostDetailComponent],
   imports: [
     CommonModule,
     PostsRoutingModule
